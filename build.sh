786c0443bdb12953c8b3a1c776d2196b4c0d77e1
Tommie McAfee <tahmmee@gmail.com>
2016-02-18 15:14:21 -0800

    Add xdcr provisioning

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-docker rm $(docker ps -q -f status=exited)
+docker rm  $(docker ps -aq)
+docker pull martin/wait
 docker build -t ansible  ansible/
 docker build -t gideon frameworks/gideon/
 docker build -t perfrunner-n1ql frameworks/perfrunner/
-docker build -t couchbase-4.1 couchbase/
+docker build -t couchbase-watson couchbase/
 docker build -t couchbase-cli frameworks/couchbase-cli
--- run.js
+++ run.js
@@ -1,13 +1,15 @@
 var Docker = require('dockerode')
     , async = require('async')
 	, fs = require('fs')
-	, assert = require('assert')
-    , waitForPort = require('wait-for-port');
+	, assert = require('assert');
 
 
-var DOCEKRIP = '192.168.99.101'
+var DOCKER_APP_ENDPOINT = '/var/run/docker.sock'
 var SERVERS = {ips: []}
 
+var docker = new Docker({socketPath: DOCKER_APP_ENDPOINT});
+
+/*
 var docker = new Docker({
 	protocol: 'https',
 	host: DOCEKRIP, 
@@ -15,10 +17,10 @@ var docker = new Docker({
 	ca: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/ca.pem'),
 	cert: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/cert.pem'),
 	key: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/key.pem')
-});
+};
+*/
 
 
-/*
 describe("Cleanup", function(){
     var _glb = {containers: []} 
 
@@ -49,23 +51,25 @@ describe("Start Cluster", function(){
         it('should start node '+name, function(done) {
             docker.createContainer({Image: 'couchbase-watson', name: name}, function(err, container){
                 if(err){done(err)}
-                var startOps = {}
-                if(i==1){
-                    startOps = {"PortBindings": {"8091/tcp": [{"HostPort": "8091"}]}}
-                }
+                i++
+                var startOps = {"PortBindings": {"8091/tcp": [{"HostPort": "809"+i}]}}
                 container.start(startOps, done)
             })
         })
     })
 
-    it('did start cluster', function(done){
-        waitForPort(DOCEKRIP, 8091, {numRetries:60}, done)
+    servers.forEach(function(name, i){
+        it('did start cluster', function(done){
+            i++
+            var dbLink = "db"+i+":db"+i
+            docker.run('martin/wait', 
+                       ["-p", "8091"], 
+                       [process.stdout], 
+                       {Tty:false, Links:[dbLink]}, done)
+        })
     })
 
 })
-
-
-*/
 
 describe("Provision Cluster", function(){
     var _glb = {hosts: []} 
@@ -76,6 +80,7 @@ describe("Provision Cluster", function(){
             async.each(containers, function(item, cb){ docker.getContainer(item.Id).inspect(function(e,d){
                 if(d.Config.Image == "couchbase-watson"){
                     _glb.hosts.push(d.NetworkSettings.IPAddress)
+                    _glb.hosts.sort()
                 }
                 cb(e)
             }) }, done)
@@ -125,8 +130,6 @@ describe("Provision Cluster", function(){
     })
 })
 
-
-/*
 describe("Phase 1 - Start data loading", function(){
     it('should start perfrunner ', function (done) {
         docker.createContainer({Image: 'perfrunner-n1ql', name: 'perf', Links:['db1:db1', 'db2:db2', 'db3:db3', 'db4:db4']}, function(err, container){
@@ -141,10 +144,9 @@ describe("Phase 1 - Start data loading", function(){
             container.start(done)
         })
     })
-})*/
+})
 
-
-describe("Phase 2: Start XDCR", function(){
+describe("Phase 2: Provision Remote Cluster", function(){
     var _glb = {hosts: []} 
     this.timeout(60000) // 1 min
 
@@ -153,15 +155,50 @@ describe("Phase 2: Start XDCR", function(){
             async.each(containers, function(item, cb){ docker.getContainer(item.Id).inspect(function(e,d){
                 if(d.Config.Image == "couchbase-watson"){
                     _glb.hosts.push(d.NetworkSettings.IPAddress)
+                    _glb.hosts.sort()
                 }
                 cb(e)
             }) }, done)
         })
     })
 
+    it('should init remote cluster', function(done){
+        var remote_host = _glb.hosts[_glb.hosts.length-1]
+        docker.run('couchbase-cli', 
+           ['./couchbase-cli', 'cluster-init', '-c', remote_host, '-u', 'Administrator', '-p', 'password', '--cluster-username', 'Administrator', '--cluster-password', 'password', '--cluster-port', '8091', '--cluster-ramsize', '300', '--services', 'data'], 
+           [process.stdout], 
+           {Tty:false}, done)
+    })
+
+    it('add nodes to remote cluster', function(done){
+     var orchestrator = _glb.hosts[_glb.hosts.length-1]
+     async.each(_glb.hosts.splice(4, 3), function(ip, cb){
+            docker.run('couchbase-cli', 
+                       ['./couchbase-cli', 'server-add', '-c', orchestrator, '-u', 'Administrator', '-p', 'password', '--server-add', ip, '--server-add-username', 'Administrator', '--server-add-password', 'password'], 
+                       [process.stdout], 
+                       {Tty:false}, cb)
+            }, done)
+    })
+
+    it('rebalance remote cluster', function(done){
+        var orchestrator = _glb.hosts[_glb.hosts.length-1]
+        docker.run('couchbase-cli', 
+               ['./couchbase-cli', 'rebalance', '-c', orchestrator, '-u', 'Administrator', '-p', 'password'], 
+               [process.stdout], 
+               {Tty:false}, done)
+    })
+
+    it('create remote bucket', function(done){
+        var remote_host = _glb.hosts[_glb.hosts.length-1]
+        docker.run('couchbase-cli', 
+           ['./couchbase-cli', 'bucket-create', '-c', remote_host, '-u', 'Administrator', '-p', 'password', '--bucket', 'bucket-1', '--bucket-ramsize', '300', '--bucket-type', 'couchbase', '--wait'], 
+           [process.stdout, process.stderr], 
+           {Tty:false}, done)    
+    })
+
     it('should setup remote replication', function(done){
         var orchestrator = _glb.hosts[0]
-        remote_host = _glb.hosts[_glb.hosts.length-1]
+        var remote_host = _glb.hosts[_glb.hosts.length-1]
         docker.run('couchbase-cli', 
                    ['./couchbase-cli', 'xdcr-setup', '-c', orchestrator, '--create', '--xdcr-cluster-name', 'remote', '--xdcr-hostname', remote_host, '--xdcr-username', 'Administrator', '--xdcr-password', 'password'], 
                    [process.stdout,  process.stderr], 
@@ -177,8 +214,10 @@ describe("Phase 2: Start XDCR", function(){
     })
 
 })
+// QUERIES
+// /tmp/env/bin/python -m perfrunner -c /root/perfrunner/clusters/systest.spec -t tests/query_dev_20M_group_by.test  --local
 
-/*
+
 describe("Phase 3 - Remote Rebalance in", function(){
     var _glb = {hosts: []} 
     this.timeout(60000) // 1 min
@@ -212,64 +251,3 @@ describe("Phase 3 - Remote Rebalance in", function(){
                {Tty:false}, done)
     })
 })
-*/
-
-
-/*
----
-# start single direction xdcr
-- hosts: orchestrator
-  vars:
-    rest_user: "Administrator"
-    rest_pass: "password"
-    bucket_to: "default"
-    bucket_from: "default"
-    cluster_name: "remote"
-    cli_bin:  "/opt/couchbase/bin/couchbase-cli"
-    internal_ip: "{{hostvars[inventory_hostname]['internal']}}"
-  remote_user: root
-  tasks:
-   - name: xdcr setup
-     shell: "{{cli_bin}} xdcr-setup -c {{internal_ip}} --create --xdcr-cluster-name={{cluster_name}} --xdcr-hostname={{groups['remote'][0]}}  --xdcr-username={{rest_user}} --xdcr-password={{rest_pass}} -u {{rest_user}} -p {{rest_pass}}"
-   - name: xdcr replicate
-     shell: "{{cli_bin}} xdcr-replicate -c {{internal_ip}} --xdcr-cluster-name={{cluster_name}}  --xdcr-from-bucket={{bucket_from}} --xdcr-to-bucket={{bucket_to}} -u {{rest_user}} -p {{rest_pass}}"
-
-
-
-// loading
-
-
-# rebalance in 1 node
-- hosts: couchbaseservers
-  vars:
-   data_path: "/data"
-   index_path: "/data"
-   rest_user: "Administrator"
-   rest_pass: "password"
-   rest_port: 8091
-   cli_bin:  "/opt/couchbase/bin/couchbase-cli"
-   internal_ip: "{{hostvars[inventory_hostname]['internal']}}"
-  remote_user: root
-  tasks:
-    - name: join cluster
-      shell: "{{cli_bin}} server-add -c {{groups['orchestrator'][0]}}:{{rest_port}} --server-add={{internal_ip}}:{{rest_port}} --server-add-username={{rest_user}} --server-add-password={{rest_pass}} -u {{rest_user}} -p {{rest_pass}}"
-      when: hostvars[inventory_hostname]['phase'] == 'primary'
-
-
-- hosts: orchestrator
-  vars:
-    rest_user: "Administrator"
-    rest_pass: "password"
-    cli_bin:  "/opt/couchbase/bin/couchbase-cli"
-    internal_ip: "{{hostvars[inventory_hostname]['internal']}}"
-  remote_user: root
-  tasks:
-   - name: rebalance in nodes
-     shell: "{{cli_bin}} rebalance -c {{internal_ip}} -u {{rest_user}} -p {{rest_pass}}"
-
-
-
-# view queries
-
-*/
-
