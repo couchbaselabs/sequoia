13302578a7bd83f713cb795698301849f958c4b1
Tommie McAfee <tahmmee@gmail.com>
2016-02-26 11:57:46 -0500

    add testrunner dockerfile

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ docker build -t gideon containers/gideon/
 docker build -t perfrunner containers/perfrunner/
 docker build -t couchbase-watson couchbase/
 docker build -t couchbase-cli containers/couchbase-cli
+docker build -t testrunner containers/testrunner
 npm install
