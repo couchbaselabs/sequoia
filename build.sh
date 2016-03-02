a5b40eadfb42692858f99e6a6dede4e5d1395040
Parag Agarwal <parag@couchbase.com>
2016-03-02 13:28:30 -0800

    tpcc::add build support

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ docker build -t perfrunner containers/perfrunner/
 docker build -t couchbase-watson couchbase/
 docker build -t couchbase-cli containers/couchbase-cli
 docker build -t testrunner containers/testrunner
+docker build -t tpcc containers/tpcc
+docker build -t ycsb containers/ycsb
 npm install
