d427240fcf35cbcb4798b8afe2984b1ef60523ef
Tommie McAfee <tahmmee@gmail.com>
2016-03-11 12:42:58 -0500

    Add template containers to improve build times
    new python and gcc containers used as base for other containers

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
-docker rm  $(docker ps -aq)
+# public images
 docker pull martin/wait
-docker build -t gideon containers/gideon/
-docker build -t perfrunner containers/perfrunner/
-docker build -t couchbase-watson containers/couchbase/
+
+# base template
+docker build -t ubuntu_gcc -f containers/templates/Dockerfile-ubuntu-gcc  containers/templates/
+docker build -t ubuntu_python -f containers/templates/Dockerfile-ubuntu-python  containers/templates/
+
+# framework containers
+docker build -t perfrunner containers/perfrunner
+docker build -t couchbase-watson containers/couchbase
 docker build -t couchbase-cli containers/couchbase-cli
 docker build -t testrunner containers/testrunner
 docker build -t tpcc containers/tpcc
 docker build -t ycsb containers/ycsb
 docker build -t pillowfight containers/pillowfight
+docker build -t gideon containers/gideon  # depends on pillowfight
+
+# build framework
 npm install
---
+++ containers/templates/Dockerfile-ubuntu-gcc
@@ -0,0 +1,3 @@
+FROM ubuntu:14.04
+RUN apt-get update
+RUN apt-get install -y gcc g++ make cmake git-core libevent-dev libev-dev
