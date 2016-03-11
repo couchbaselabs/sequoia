8b2dcadb98f2b3b9a4c5ba9c542330e72159ff70
Tommie McAfee <tahmmee@gmail.com>
2016-03-11 11:04:15 -0500

    move couchbase dir into containers dir

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ docker rm  $(docker ps -aq)
 docker pull martin/wait
 docker build -t gideon containers/gideon/
 docker build -t perfrunner containers/perfrunner/
-docker build -t couchbase-watson couchbase/
+docker build -t couchbase-watson containers/couchbase/
 docker build -t couchbase-cli containers/couchbase-cli
 docker build -t testrunner containers/testrunner
 docker build -t tpcc containers/tpcc
