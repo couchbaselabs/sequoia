9ccdc9f54822b5b5a86230765f827874ea1bbc83
Tommie McAfee <tahmmee@gmail.com>
2016-04-14 10:30:17 -0400

    use flags package for reading test/scope file

--- run.go
+++ run.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"flag"
 	S "github.com/couchbaselabs/sequoia/lib"
-	"os"
 )
 
 func main() {
 
+	scopeFile := flag.String("scope", "tests/simple/scope_small.yml", "scope spec filename")
+	testFile := flag.String("test", "tests/simple/test_simple.yml", "test spec filename")
+	configFile := flag.String("config", "config.yml", "test config filename")
+	flag.Parse()
+
 	// parse
-	config := S.NewConfigSpec(os.Args[1])
+	config := S.NewConfigSpec(*configFile)
+	config.Scope = *scopeFile
+	config.Test = *testFile
 	scope := S.NewScope(config)
 	test := S.NewTestSpec(config)
 
