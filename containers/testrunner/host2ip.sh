53b81ffb77ee88cb49fbf52e385f557cade3fda9
Tommie McAfee <tahmmee@gmail.com>
2016-05-07 00:54:27 -0400

    update testrunner container internal linking
    it's known testrunner will run with linked containers,
    so updating the resolve scripts to fill ips dynamically

--- containers/testrunner/host2ip.sh
+++ containers/testrunner/host2ip.sh
@@ -1,6 +1,9 @@
 FILE=$1
+CONTAINER_IPS=($(cat /etc/hosts | grep container | awk '{print $1}' | sort))
+i=0
 for host in $( grep ip: $FILE | awk -F ":" '{print $2}' ); do
-  addr=$(getent hosts $host | cut -d' ' -f1)
+  addr=${CONTAINER_IPS[i]}
   echo $host."->".$addr
   sed -i "s/$host/$addr/g" $FILE
+ ((i++))
 done
