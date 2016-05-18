bc37adea865741690e9bc5842596404c30a8defe
Tommie McAfee <tahmmee@gmail.com>
2016-05-18 15:17:56 -0400

    add view request looping to curloop

---
+++ containers/curloop/view.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+HOST=$1
+BUCKET=$2
+DDOC=$3
+VIEW=$4
+PARAMS=$5
+AUTH=${6:-Administrator:password}
+
+URL=$HOST/$BUCKET/_design/$DDOC/_view/$VIEW?$PARAMS 
+echo $URL
+while [ 1 ] ; do
+  curl -s -u $AUTH $URL
+done
