fde761bd301d5ba87fee35f3ba4b16e05fdd6985
Tommie McAfee <tahmmee@gmail.com>
2016-05-18 15:19:06 -0400

    remove count from n1ql query loop

--- containers/curloop/n1ql.sh
+++ containers/curloop/n1ql.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 HOST=$1
 Q=$2
-if [ -n "$3" ]; then
-  seq 1 $3 | xargs -I '{}'  curl -s $1/query/service -d "statement=$2"
-else
-  while [ 1 ] ; do
-    curl -s $1/query/service -d "statement=$2"
-  done
-fi
+while [ 1 ] ; do
+  curl -s $HOST/query/service -d "statement=$Q"
+done
 
