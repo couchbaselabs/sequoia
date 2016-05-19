faad37e1f2fe78581704279f113ca24771b410a6
Tommie McAfee <tahmmee@gmail.com>
2016-05-18 22:42:00 -0400

    log curloop responses to file

--- containers/curloop/n1ql.sh
+++ containers/curloop/n1ql.sh
@@ -2,6 +2,6 @@
 HOST=$1
 Q=$2
 while [ 1 ] ; do
-  curl -s $HOST/query/service -d "statement=$Q"
+  curl -s $HOST/query/service -d "statement=$Q" > logs.txt
 done
 
--- containers/curloop/view.sh
+++ containers/curloop/view.sh
@@ -9,5 +9,5 @@ AUTH=${6:-Administrator:password}
 URL=$HOST/$BUCKET/_design/$DDOC/_view/$VIEW?$PARAMS 
 echo $URL
 while [ 1 ] ; do
-  curl -s -u $AUTH $URL
+  curl -s -u $AUTH $URL > logs.txt
 done
