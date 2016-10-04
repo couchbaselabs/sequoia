fce93f9dea7565eccc45397731e04c0f2c9e5f3c
andreibaranouski <andrei.baranouski@gmail.com>
2016-10-04 22:56:35 +0300

    add cbas statements

--- containers/curloop/cbas.sh
+++ containers/curloop/cbas.sh
@@ -6,4 +6,4 @@ STATEMENT=${@}
 #echo $ID
 echo $HOST
 echo $STATEMENT
-curl -s --data pretty=true --data format=CLEAN_JSON --data-urlencode "statement=$STATEMENT" http://$HOST:8095/analytics/service -v > logs.txt
+curl -s --data pretty=true --data format=CLEAN_JSON --data-urlencode "statement=$STATEMENT" http://$HOST:8095/analytics/service -v #> logs.txt
--- containers/curloop/cbasloop.sh
+++ containers/curloop/cbasloop.sh
@@ -4,5 +4,5 @@
 HOST="172.17.0.4"
 STATEMENT=$1
 while [ 1 ] ; do
-  curl -s --data pretty=true --data format=CLEAN_JSON --data-urlencode "statement=$STATEMENT" http://$HOST:8095/analytics/service -v > logs.txt
+  curl -s --data pretty=true --data format=CLEAN_JSON --data-urlencode "statement=$STATEMENT" http://$HOST:8095/analytics/service -v #> logs.txt
 done
