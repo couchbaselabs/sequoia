e0cf8e1800d79d8d3a95849085e32048a8314c05
andreibaranouski <andrei.baranouski@gmail.com>
2016-10-04 13:23:07 +0300

    new iteration

--- containers/curloop/cbas.sh
+++ containers/curloop/cbas.sh
@@ -3,7 +3,7 @@
 #HOST=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $ID`
 HOST="172.17.0.4"
 STATEMENT=$1
-echo $ID
+#echo $ID
 echo $HOST
 echo $STATEMENT
 curl -s --data pretty=true --data format=CLEAN_JSON --data-urlencode '$STATEMENT' http://$HOST:8095/analytics/service -v > logs.txt
