530c2f16cfc8171d8543688eb328bc5d5e192198
andreibaranouski <andrei.baranouski@gmail.com>
2016-10-24 17:57:59 +0300

    add new queries

--- containers/curloop/cbas.sh
+++ containers/curloop/cbas.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #ID=`docker ps -a | grep "Up" | grep "sequoiatools/cbas" | awk '{print $1}'`
 #HOST=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $ID`
-HOST="172.17.0.5"  # we need run 'docker rm -fv $(docker ps -qa)' before test
+HOST="172.17.0.4"  # we need run 'docker rm -fv $(docker ps -qa)' before test
 STATEMENT=$@
 STATEMENT=${STATEMENT//\\/""}  # remove \ slashes
 STATEMENT=${STATEMENT//\'NUM/""}  # to use nums in expression
-#echo "curl -s --data pretty=true --data-urlencode \"statement=$STATEMENT\" http://$HOST:8095/analytics/service -v"
+echo "curl -s --data pretty=true --data-urlencode \"statement=$STATEMENT\" http://$HOST:8095/analytics/service -v"
 curl -s --data pretty=true --data format=CLEAN_JSON --data-urlencode "statement=$STATEMENT" http://$HOST:8095/analytics/service #> log
