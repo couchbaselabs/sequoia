d3f60f76ca5fcc68d44049f81d44de9b2103c0bc
Tommie McAfee <tahmmee@gmail.com>
2017-03-30 13:59:06 -0400

    support for running testrunner in swarm

--- containers/testrunner/host2ip.sh
+++ containers/testrunner/host2ip.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
 FILE=$1
+
 CONTAINER_IPS=($(cat /etc/hosts | grep container | awk '{print $1}' | sort))
+if [ -n "$SWARM_HOSTS" ]; then
+   # override
+   CONTAINER_IPS=($(echo $SWARM_HOSTS | sed 's/,/\n/g'))
+fi
+
+
 i=0
 for host in $( grep ip: $FILE | awk -F ":" '{print $2}' ); do
   addr=${CONTAINER_IPS[i]}
