f1cc02c9ed5c50c0372cdfb73a93b615425f02d8
Tommie McAfee <tahmmee@gmail.com>
2017-04-25 17:22:32 -0400

    add sdkd Dockerfile
    This builds an sdk image for running sdkdclient tests via docker
    
    NOTE: building this container requires access to sdk repo
    the following folders should be cloned before doing a build here
     * git@github.com:couchbaselabs/sdkdclient-ng.git
     * git@github.com:couchbase/sdkd-cpp.git

---
+++ containers/sdk/host2ip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+FILE=$1
+
+CONTAINER_IPS=($(cat /etc/hosts | grep container | awk '{print $1}' | sort))
+if [ -n "$SWARM_HOSTS" ]; then
+   # override
+   CONTAINER_IPS=($(echo $SWARM_HOSTS | sed 's/,/\n/g'))
+fi
+
+i=0
+for host in $( grep "node =" $FILE | awk -F "=" '{print $2}' ); do
+  addr=${CONTAINER_IPS[i]}
+  echo $host" -> "$addr
+  sed -i "0,/node = $host/s//node = $addr:8091/" $FILE
+  ((i++))
+done
