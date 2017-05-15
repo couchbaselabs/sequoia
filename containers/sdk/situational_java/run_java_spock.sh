56e4314752582a24390f1e97c473d9331d0a13f7
Jaekwon Park <jaekwon.park@couchbase.com>
2017-05-15 00:35:30 -0700

    removed port export option due to port conflict while running

--- containers/sdk/situational_java/run_java_spock.sh
+++ containers/sdk/situational_java/run_java_spock.sh
@@ -8,4 +8,5 @@ git clone -b for_docker git@github.com:couchbaselabs/sdkdclient-ng.git || true
 docker build -t sequoiatools/sdk .
 
 export CONTAINER_NAME=sdkdclient
-~/bin/sequoia sdk -expose_ports -command "-I spock-basic.ini --install-skip true --rebound 90 --bucket-password="password" --testsuite-test=Rb1Swap --testsuite-variants=HYBRID -d all:debug -A S3Creds_tmp -C share/rexec --rexec_path=/root/sdkd-java/run-sdkd-java --rexec_port=8050" -container_name ${CONTAINER_NAME}
+#~/bin/sequoia sdk -expose_ports -command "-I spock-basic.ini --install-skip true --rebound 90 --bucket-password="password" --testsuite-test=Rb1Swap --testsuite-variants=HYBRID -d all:debug -A S3Creds_tmp -C share/rexec --rexec_path=/root/sdkd-java/run-sdkd-java --rexec_port=8050" -container_name ${CONTAINER_NAME}
+~/bin/sequoia sdk -command "-I spock-basic.ini --install-skip true --rebound 90 --bucket-password="password" --testsuite-test=Rb1Swap --testsuite-variants=HYBRID -d all:debug -A S3Creds_tmp -C share/rexec --rexec_path=/root/sdkd-java/run-sdkd-java --rexec_port=8050" -container_name ${CONTAINER_NAME}
