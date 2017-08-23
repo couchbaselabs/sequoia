2e8f633fbedd8c2f724a40cde289b283442d8c47
Mahesh Mandhare <mahesh.mandhare@couchbase.com>
2017-08-23 15:00:12 +0530

    Adding replica-failover-rebalance test for 2i for local laptop cluster Change in curloop/n1ql.sh to include credentials Scale related changes in local_gideon test for 2i
    Change-Id: If5ad41ad0b384a979fbfb2ab75261963dcd6e988
    Reviewed-on: http://review.couchbase.org/82629
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>
    Tested-by: Mahesh Mandhare <mahesh.mandhare@couchbase.com>

--- containers/curloop/n1ql.sh
+++ containers/curloop/n1ql.sh
@@ -2,6 +2,6 @@
 HOST=$1
 Q=$2
 while [ 1 ] ; do
-  curl -s $HOST/query/service -d "statement=$Q" > logs.txt
+  curl -s $HOST/query/service -d "statement=$Q" -u Administrator:password > logs.txt
 done
 
