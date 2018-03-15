fc35c4d1ef22b568c85bd30097fc238c331ae110
vikas chaudhary <vikas.chaudhary@couchbase.com>
2018-03-15 10:56:15 +0530

    Autofailover: starting stoping service in parallel
    Change-Id: I130bb9f8449ec5d71cdbbc6a3c3a822e8d5c5cf8
    Reviewed-on: http://review.couchbase.org/90977
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>
    Tested-by: vikas chaudhary <vikas.chaudhary@couchbase.com>

--- containers/cbutil/cbinit.py
+++ containers/cbutil/cbinit.py
@@ -1,6 +1,8 @@
 import paramiko
 import sys
 import select
+from threading import Thread
+
 
 class CBInit():
     def run(self):
@@ -25,11 +27,41 @@ class CBInit():
                 self.command="service couchbase-server stop"
         self.execute_command(self.command)
 
-    def execute_command(self, command):
+    def run_parallel(self):
+        self.hostname = sys.argv[1]
+        ips=self.hostname.split(',')
+        threads = []
+        for i in range(len(ips)):
+            host=ips[i]
+            threads.append(Thread(target=self.create_command, args=(host,)))
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+
+    def create_command(self, ip):
+        self.sshusername = sys.argv[2]
+        self.sshpassword = sys.argv[3]
+        self.operation = sys.argv[4]
+        version, err = self.execute_command("cat /etc/*release | grep -w NAME=",ip)
+
+        if self.operation == "start":
+            if version[0].__contains__("CentOS Linux"):
+                self.command = "systemctl start couchbase-server.service"
+            elif version[0].__contains__("Ubuntu"):
+                self.command = "service couchbase-server start"
+        elif self.operation == "stop":
+            if version[0].__contains__("CentOS Linux"):
+                self.command = "systemctl stop couchbase-server.service"
+            elif version[0].__contains__("Ubuntu"):
+                self.command = "service couchbase-server stop"
+        self.execute_command(self.command,ip)
+
+    def execute_command(self, command,host):
 
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(self.hostname, username=self.sshusername, password=self.sshpassword)
+        ssh.connect(host, username=self.sshusername, password=self.sshpassword)
 
         print "Executing : {0}".format(command)
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command)
@@ -53,4 +85,4 @@ class CBInit():
 
 
 if __name__ == '__main__':
-    CBInit().run()
+    CBInit().run_parallel()
