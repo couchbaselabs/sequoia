cb98d73f40fa23d82336e3b9bf681221851a3d2e
girish.benakappa <girish.benakappa@couchbase.com>
2019-09-30 12:57:20 -0700

    sytem test for array indexes with ycsb
    Change-Id: I73f523900a5d1027f220aff5f0b31756bc54ae3a
    Reviewed-on: http://review.couchbase.org/115623
    Tested-by: Girish Benakappa <girish.benakappa@couchbase.com>
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>

---
+++ containers/ycsb-soe/run_ycsb.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+record_count=$1
+num_threads=$2
+hostname=$3
+bucket_name=$4
+
+service memcached start
+
+cd /YCSB
+
+cmd="bin/ycsb load couchbase2 -P workloads/soe/workloadsmix3 -p writeallfields=true -threads $num_threads -p target=0 -p fieldlength=100 -p fieldcount=10 -p couchbase.host=$hostname -p couchbase.bucket=$bucket_name -p couchbase.upsert=true -p couchbase.epoll=true -p couchbase.boost=48 -p couchbase.kvEndpoints=1 -p couchbase.sslMode=none -p couchbase.certKeystoreFile=../certificates/data.keystore -p couchbase.certKeystorePassword=storepass -p couchbase.password=password -p exportfile=ycsb_run_3.log -p couchbase.persistTo=0 -p couchbase.replicateTo=0  -p operationcount=500000000 -p totalrecordcount=$record_count  -p recordcount=$record_count -p insertstart=400 2>&1 | tee -a ycsb_load.log"
+echo $cmd | tee ycsb_load.log
+eval $cmd
+cmd="bin/ycsb run couchbase2 -P workloads/soe/workloadsmix3 -p writeallfields=true -threads $num_threads -p target=0 -p fieldlength=100 -p fieldcount=10 -p couchbase.host=$hostname -p couchbase.bucket=$bucket_name -p couchbase.upsert=true -p couchbase.epoll=true -p couchbase.boost=48 -p couchbase.kvEndpoints=1 -p couchbase.sslMode=none -p couchbase.certKeystoreFile=../certificates/data.keystore -p couchbase.certKeystorePassword=storepass -p couchbase.password=password -p exportfile=ycsb_run_3.log -p couchbase.persistTo=0 -p couchbase.replicateTo=0  -p operationcount=500000000 -p totalrecordcount=$record_count  -p recordcount=$record_count -p insertstart=400 2>&1 | tee -a ycsb_run.log"
+echo $cmd | tee ycsb_run.log
+eval $cmd
