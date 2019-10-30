a3779931a33acc33bdec2fd879cda115ca8296ba
Ajay Bhullar <ajay.bhullar@couchbase.com>
2019-10-30 16:18:24 -0700

    Fixes for volume test. Changes to container for wait_for_indexes build to wait for ingestion.
    Change-Id: Ic91a619daa3921340ca3cd26926954106a21fc8a
    Reviewed-on: http://review.couchbase.org/117161
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>
    Tested-by: Ajay Bhullar <ajay.bhullar@couchbase.com>

--- containers/wait_for_idx_build_complete/wait_for_idx_build_complete.py
+++ containers/wait_for_idx_build_complete/wait_for_idx_build_complete.py
@@ -35,16 +35,123 @@ class WaitForAllIndexBuildComplete():
         else:
             response.raise_for_status()
 
+    def check_index_pending(self, timeout=3600):
+        index_node_list = []
+        index_host_list = []
+        cluster_config_endpoint = "http://" + sys.argv[1] + ":8091/pools/default"
+        cluster_username = sys.argv[2]
+        cluster_password = sys.argv[3]
+
+        index_status_endpoint = "http://" + sys.argv[1] + ":9102/getIndexStatus"
+
+        # Get status for all indexes
+        response = requests.get(index_status_endpoint, auth=(cluster_username, cluster_password), verify=True)
+
+        if response.ok:
+            response = json.loads(response.content)
+            # Find each indexer node that has indexes present
+            if "status" in response:
+                for index in response['status']:
+                    for host in index['hosts']:
+                        if host not in index_host_list:
+                            host_name = host.split(":")[0]
+                            index_host_list.append(host_name)
+        else:
+            response.raise_for_status()
+
+
+        # Get list of indexer nodes
+        response = requests.get(cluster_config_endpoint, auth=(cluster_username, cluster_password), verify=True)
+
+        if response.ok:
+            response = json.loads(response.content)
+            for nodes in response['nodes']:
+                if "index" in nodes['services']:
+                    node_name = nodes['hostname'].split(":")[0]
+                    # Add indexer nodes with indexes on them to list of nodes to be checked
+                    if node_name in index_host_list:
+                        index_node_list.append(node_name)
+        else:
+            response.raise_for_status()
+
+        timedout = self.check_index_nodes(index_node_list,timeout)
+
+        print "Sleep for 120 seconds to see if indexes are fully done being built"
+        # Sleep for some time and then again make sure that there are no docs_pending in any indexes
+        if not timedout:
+            time.sleep(120)
+        else:
+            raise Exception("Timeout has been reached and ingestion has not completed!")
+
+        timedout = self.check_index_nodes(index_node_list,timeout)
+
+        if timedout:
+            raise Exception("Timeout has been reached and ingestion has not completed!")
+
+        return all_indexes_caught_up
+
+    def check_index_nodes(self, index_node_list = [], timeout=3600):
+        timedout = False
+        st_time = time.time()
+        cluster_username = sys.argv[2]
+        cluster_password = sys.argv[3]
+        for index_node in index_node_list:
+            all_indexes_caught_up = False
+            if timedout:
+                raise Exception("Timeout has been reached and ingestion has not completed!")
+            index_stats = {}
+            index_stats_endpoint = "http://" + index_node + ":9102/stats"
+            while not all_indexes_caught_up:
+                # Get status for all indexes
+                response = requests.get(index_stats_endpoint, auth=(cluster_username, cluster_password), verify=True)
+
+                if response.ok:
+                    response = json.loads(response.content)
+                    for item in response:
+                        if "num_docs_pending" in item:
+                            if response[item] == 0:
+                                index_stats[item] = True
+                            else:
+                                index_stats[item] = False
+                else:
+                    response.raise_for_status()
+                    break
+
+                if st_time + timeout < time.time():
+                    timedout = True
+                    break
+
+                for key in index_stats.keys():
+                    if index_stats[key]:
+                        all_indexes_caught_up = True
+                    else:
+                        all_indexes_caught_up = False
+        return timedout
 
 if __name__ == '__main__':
     all_indexes_built = False
-    while not all_indexes_built:
+    check_index_stats = False
+    all_indexes_caught_up = False
+    if len(sys.argv) > 4:
+        if sys.argv[4] == "wait_for_index_ingestion":
+            check_index_stats = True
+    if check_index_stats:
         try:
-            all_indexes_built = WaitForAllIndexBuildComplete().check_index_status()
+            if len(sys.argv) != 6:
+                raise Exception("This script expects 5 arguments - index node ip, username, password, wait_for_index_ingestion, timeout")
+            else:
+                all_indexes_caught_up = WaitForAllIndexBuildComplete().check_index_pending(int(sys.argv[5]))
         except Exception as e:
-            print "Error getting index status"
-            break
+            print "Error while waiting for ingestion"
+            print str(e)
+    else:
+        while not all_indexes_built:
+            try:
+                all_indexes_built = WaitForAllIndexBuildComplete().check_index_status()
+            except Exception as e:
+                print "Error getting index status"
+                break
 
-        # Sleep for 1 min
-        time.sleep(60)
+            # Sleep for 1 min
+            time.sleep(60)
 
