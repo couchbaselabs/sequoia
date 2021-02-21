578938d32e480fa7a4a5222c7e0f120f36018583
Girish Benakappa <girish.benakappa@couchbase.com>
2021-02-20 20:30:10 -0800

    stop loading before rebalance in
    Change-Id: I7bc07c0de5f3685e73dc3f13cb28583a0d002ee0
    Reviewed-on: http://review.couchbase.org/c/sequoia/+/146630
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>
    Tested-by: Girish Benakappa <girish.benakappa@couchbase.com>

--- containers/ftsindexmanager/ftsIndexManager.py
+++ containers/ftsindexmanager/ftsIndexManager.py
@@ -8,7 +8,7 @@ import threading
 from concurrent.futures import as_completed
 from concurrent.futures.thread import ThreadPoolExecutor
 from datetime import datetime
-from http.client import RemoteDisconnected
+from http.client import RemoteDisconnected, IncompleteRead
 
 from couchbase.cluster import Cluster, ClusterOptions, QueryOptions, ClusterTimeoutOptions
 from couchbase.exceptions import QueryException, QueryIndexAlreadyExistsException, TimeoutException
@@ -25,6 +25,7 @@ import requests
 import time
 import httplib2
 import json
+import paramiko
 
 ## Constants
 
@@ -69,7 +70,13 @@ HOTEL_DS_FIELDS = [
                         "operator": "or"
                     }
                 ]
-            }]},
+            }],
+        "flex_queries": ["country = \"Moldova\"",
+                         "country = \"Cape Verde\"",
+                         "country like \"Jord*\"",
+                         "country like \"Jord*\" or country = \"Cape Verde\""
+                         ]
+    },
     {
         "name": "free_parking",
         "type": "boolean",
@@ -82,7 +89,10 @@ HOTEL_DS_FIELDS = [
             {
                 "bool": False,
                 "field": "free_parking"
-            }]},
+            }],
+        "flex_queries": ["free_parking = False",
+                         "free_parking = True"]
+    },
     {
         "name": "price",
         "type": "number",
@@ -96,7 +106,11 @@ HOTEL_DS_FIELDS = [
                 "inclusive_min": False,
                 "inclusive_max": False,
                 "field": "price"
-            }]},
+            }],
+        "flex_queries": ["price = 849",
+                         "price > 1 and price < 1000"
+                         ]
+    },
     {
         "name": "public_likes",
         "type": "text",
@@ -114,7 +128,11 @@ HOTEL_DS_FIELDS = [
             {
                 "wildcard": "Dain*",
                 "field": "public_likes"
-            }]},
+            }],
+        "flex_queries": ["public_likes = \"Daina Cassin\"",
+                         "public_likes like \"Dain%\""
+                         ]
+    },
     {
         "name": "reviews.ratings.Overall",
         "type": "number",
@@ -128,7 +146,9 @@ HOTEL_DS_FIELDS = [
                 "inclusive_min": False,
                 "inclusive_max": False,
                 "field": "reviews.ratings.Overall"
-            }]},
+            }],
+        "flex_queries": ["ANY v in reviews.ratings.Overall SATISFIES v = 3 END"]
+    },
     {
         "name": "reviews.date",
         "type": "datetime",
@@ -147,7 +167,9 @@ HOTEL_DS_FIELDS = [
                 "inclusive_start": False,
                 "inclusive_end": False,
                 "field": "reviews.date"
-            }]}
+            }],
+        "flex_queries": ["ANY v in reviews.date SATISFIES v > \"2001-10-09\" and v < \"2020-12-18\" END"]
+    }
 ]
 
 # Some constants
@@ -164,6 +186,9 @@ class FTSIndexManager:
         parser.add_argument("-u", "--username", help="Couchbase Server Cluster Username")
         parser.add_argument("-p", "--password", help="Couchbase Server Cluster Password")
         parser.add_argument("-b", "--bucket", help="Bucket name on which indexes are to be created")
+        parser.add_argument("-m", "--index_partition_map", help="Index partion map for create index",
+                            default="1:1:20,2:2:6")
+        parser.add_argument("-s", "--scale", help="scale for Index partion map for create index", default=1)
         parser.add_argument("-i", "--num_indexes", type=int,
                             help="Number of indexes to be created on collections for a bucket, if action = create_index. "
                                  "If action=drop_index, number of indexes to be dropped per collection of bucket")
@@ -185,8 +210,8 @@ class FTSIndexManager:
         parser.add_argument("-vt", "--validation_timeout", type=int, default=1200,
                             help="Timeout for item_count_check")
         parser.add_argument("-a", "--action",
-                            choices=["create_index", "run_queries", "delete_all_indexes", "create_index_loop",
-                                     "item_count_check", "active_queries_check"],
+                            choices=["create_index", "create_index_from_map", "run_queries", "delete_all_indexes",
+                                     "create_index_loop", "item_count_check", "active_queries_check", "run_flex_queries"],
                             help="Choose an action to be performed. Valid actions : create_index, run_queries, "
                                  "delete_all_indexes, create_index_loop, item_count_check",
                             default="create_index")
@@ -205,6 +230,8 @@ class FTSIndexManager:
         self.print_interval = args.print_interval
         self.interval = args.interval
         self.timeout = args.timeout
+        self.index_partition_map = args.index_partition_map
+        self.scale = args.scale
         self.num_queries_per_worker = args.num_queries_per_worker
         self.validation_timeout = args.validation_timeout
 
@@ -282,6 +309,53 @@ class FTSIndexManager:
             for index_name in index_list:
                 self.log_active_queries(index_name)
 
+    def run_flex_queries(self):
+        threads = []
+        queries_run = 0
+        queries_passed = 0
+        queries_failed = 0
+        with ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
+            # Establish timeout. If timeout > 0, run in infinite loop
+            end_time = 0
+            print_time = 0
+            if self.duration > 0:
+                end_time = time.time() + self.duration
+
+            if self.print_interval > 0:
+                print_time = time.time() + self.print_interval
+
+            while True:
+                random.seed(datetime.now())
+                for i in range(self.num_queries_per_worker):
+                    threads.append(executor.submit(self.generate_and_run_flex_query))
+                    time.sleep(5)
+
+                for task in as_completed(threads):
+                    result = task.result()
+                    queries_run += 1
+                    if result:
+                        queries_passed += 1
+                    else:
+                        queries_failed += 1
+
+                # Print result summary if the print interval has passed
+                if self.print_interval > 0 and time.time() > print_time:
+                    self.log.info(
+                        "======== Queries Run = {0} | Queries Passed = {1} | Queries Failed = {2} ========".format(
+                            queries_run, queries_passed, queries_failed))
+                    # Set next time to print result summary
+                    print_time = time.time() + self.print_interval
+
+                # Exit if timed out
+                if self.duration > 0 and time.time() > end_time:
+                    break
+
+                # Wait for 1 min before submitting next set of threads
+                alive_threads = len(threading.enumerate())
+                if alive_threads > 5:
+                    self.log.info("Waiting for {0} threads to complete...".format(len(threads)))
+                    time.sleep(60)
+
     def log_active_queries(self, index_name):
         try:
             status, content, response = self.http_request(self.node_addr, FTS_PORT,
@@ -332,6 +406,51 @@ class FTSIndexManager:
             raise Exception("There were errors in the item count check phase - \n{0}".format(errors))
         else:
             self.log.info("Item check count passed. No discrepancies seen.")
+
+    def create_fts_indexes_from_map_for_bucket(self):
+        coll_list = self.get_all_collections()
+        multi_coll_scopes = self.get_all_scopes_with_multiple_collections()
+        partition_map_list = self.index_partition_map.split(",")
+        for index_map in partition_map_list:
+            num_indexes, num_replicas, num_partitions = index_map.split(":")
+            num_indexes = int(self.scale) * int(num_indexes)
+            for i in range(num_indexes):
+                random.seed(datetime.now())
+                collections = []
+                if len(multi_coll_scopes) > 0:
+                    # Select if to create single collection index or multi-collection (25% chance for multi collection idx)
+                    index_type = random.choice(["single", "single", "single", "multi"])
+                else:
+                    index_type = "single"
+
+                if index_type == "single":
+                    collections.append(random.choice(coll_list))
+                else:
+                    # Select a scope with multiple collections first
+                    scope_obj = random.choice(multi_coll_scopes)
+                    scope_name = list(scope_obj.keys())[0]
+                    num_coll = len(scope_obj[scope_name])
+                    # Now randomly select a subset of random number collections from that scope
+                    collections = random.sample(population=scope_obj[scope_name], k=random.randint(1, num_coll))
+
+                self.log.info("===== Creating {1} FTS index on {0} =====".format(collections, index_type))
+                status, content, response, idx_name = self.create_fts_index_on_collections(collections,
+                                                                                           num_replica=int(num_replicas),
+                                                                                           num_partitions=int(num_partitions))
+
+                if not status:
+                    self.log.info("Content = {0} \nResponse = {1}".format(content, response))
+                    self.log.info("Index creation on {0} did not succeed. Pls check logs.".format(collections))
+
+                # Remove the scope or collection on which the index was created
+                if index_type == "single":
+                    coll_list.remove(collections[0])
+                else:
+                    multi_coll_scopes.remove(scope_obj)
+
+            ### TO - DO
+            # Validate if all indexes have been created
+            self.get_fts_index_list()
 
     """
     Create n number of indexes for the specified bucket. These indexes could be on a single or multiple collections
@@ -418,7 +537,10 @@ class FTSIndexManager:
             if not status:
                 self.log.info("Content = {0} \nResponse = {1}".format(content, response))
                 self.log.info("Index creation on {0} did not succeed. Pls check logs.".format(collections))
+                #command = f'yum install tcpdump -y;timeout 600 tcpdump -W 1 -G 300 -w tcp_dump_file_{idx_name}.pcap -s 0 port 8094'
+                #self.execute_command(command, self.node_addr, "root", "couchbase")
             else:
+                self.log.info(f'Index creation on {idx_name} succeeded. Content = {content}, Response = {response}')
                 time.sleep(240)
                 self.log.info(f'Deleting index {idx_name}')
                 uri = "/api/index/" + idx_name
@@ -432,8 +554,47 @@ class FTSIndexManager:
                 break
             count += 1
 
-        # Wait for the interval before doing the next CRUD operation
-        time.sleep(interval)
+            # Wait for the interval before doing the next CRUD operation
+            time.sleep(interval)
+
+    def execute_command(self, command, hostname, ssh_username, ssh_password):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname, username=ssh_username, password=ssh_password,
+                    timeout=120, banner_timeout=120)
+
+        channel = ssh.get_transport().open_session()
+        channel.get_pty()
+        channel.settimeout(900)
+        stdin = channel.makefile('wb')
+        stdout = channel.makefile('rb')
+        stderro = channel.makefile_stderr('rb')
+
+        self.log.info(f'Running {command} on node {hostname}')
+        channel.exec_command(command)
+        data = channel.recv(1024)
+        temp = ""
+        while data:
+            temp += str(data)
+            data = channel.recv(1024)
+        channel.close()
+        stdin.close()
+
+        output = []
+        error = []
+        for line in stdout.read().splitlines():
+            if "No such file or directory" not in line:
+                output.append(line)
+        for line in stderro.read().splitlines():
+            error.append(line)
+        if temp:
+            line = temp.splitlines()
+            output.extend(line)
+        stdout.close()
+        stderro.close()
+
+        ssh.close()
+        return len(output), output, error
 
     """
     Create FTS indexes on a given bucket with collections
@@ -452,7 +613,7 @@ class FTSIndexManager:
     
     """
 
-    def create_fts_index_on_collections(self, collections, count=None):
+    def create_fts_index_on_collections(self, collections, count=None, num_replica=None, num_partitions=None):
         random.seed(datetime.now())
         # Randomly select fields to create the index on
         ds_fields = copy.deepcopy(HOTEL_DS_FIELDS)
@@ -469,8 +630,10 @@ class FTSIndexManager:
         self.log.debug("***********  Fields selected for index : {0}".format(index_fields))
 
         # Randomly choose number of partitions and replicas
-        num_partitions = random.randint(2, self.max_num_partitions)
-        num_replica = random.randint(0, self.max_num_replica)
+        if not num_partitions:
+            num_partitions = random.randint(2, self.max_num_partitions)
+        if not num_replica:
+            num_replica = random.randint(0, self.max_num_replica)
 
         # Generate index name. Index names will also have field codes so that the query runner can decode the fields used in the index.
         for i in range(5):
@@ -801,6 +964,44 @@ class FTSIndexManager:
                     time.sleep(60)
 
     """
+    Run Flex queries on random index with random fields
+    """
+
+    def generate_and_run_flex_query(self):
+        index_names = self.get_fts_index_list(self.bucket_name)
+
+        try:
+            index_name = random.choice(index_names)
+        except Exception as e:
+            self.log.info("Exception fetching index names : {0} - {1}".format(index_names, str(e)))
+            return False
+        if index_name:
+            index_fields = self.get_index_fields(index_name)
+            try:
+                index_field = random.choice(index_fields)
+            except Exception as e:
+                self.log.info(
+                    "Exception fetching index fields {0} for index  : {1} - {2}".format(index_fields, index_name,
+                                                                                        str(e)))
+                return False
+        else:
+            self.log.info("Index name selected for running query is empty. Discarding attempt to run query")
+            return False
+        query = random.choice(index_field["flex_queries"])
+        self.log.info("--------------- Running query {1} on {0} ---------------".format(index_name, query))
+        status = self.run_flex_query(index_name, query)
+
+        try:
+            if status:
+                self.log.info(f'Index name: {index_name}, Query: {query}, Status: {status}')
+        except TypeError as terr:
+            self.log.info(str(terr))
+        except:
+            self.log.error("Unexpected error :", sys.exc_info()[0])
+
+        return status
+
+    """
     Run FTS queries on random index with random fields
     """
 
@@ -888,6 +1089,37 @@ class FTSIndexManager:
         return status, content, response
 
     """
+    Run single Flex query
+    """
+
+    def run_flex_query(self, index_name, query):
+        index_hint = "USE INDEX (USING FTS, USING GSI)"
+        status, content, response = self.http_request(self.node_addr, FTS_PORT, "/api/index/{0}".format(index_name))
+        types = content["indexDef"]["params"]["mapping"]["types"]
+        bucket_name = content["indexDef"]["sourceName"]
+        collection_list = list(types.keys())
+        collection_to_query = random.choice(collection_list)
+        scope, collection = collection_to_query.split(".")
+        keyspace_name_for_query = "`" + bucket_name + "`.`" + scope + "`.`" + collection + "`"
+
+        flex_query = f'select meta().id from {keyspace_name_for_query} {index_hint} where {query} limit 1000'
+        self.log.info(f'flex query: {flex_query}')
+        try:
+            status, results, queryResult = self._execute_query(flex_query)
+            if status is not None:
+                self.log.info(f'For {query}, Status: {status}, Length of results: {len(results)}')
+            else:
+                self.log.info("Got an error retrieving stat from query via n1ql with query - {0}. Status : {1} ".
+                              format(flex_query, status))
+        except Exception as e:
+            self.log.info("Got an error retrieving stat from query via n1ql with query - {0}. Exception : {1} ".
+                          format(flex_query, str(e)))
+
+        return status
+
+
+
+    """
     Generic method to perform a REST call
     """
 
@@ -906,7 +1138,7 @@ class FTSIndexManager:
         try:
             response, content = http.request(uri=url, method=method, headers=headers, body=body)
 
-        except (RemoteDisconnected, httplib2.HttpLib2Error, socket.error) as ex:
+        except (RemoteDisconnected, httplib2.HttpLib2Error, socket.error, IncompleteRead) as ex:
             self.log.info(f'{url}, {body}, {ex}')
             return False, None, None
 
@@ -979,6 +1211,8 @@ if __name__ == '__main__':
 
     if ftsIndexMgr.action == "create_index":
         ftsIndexMgr.create_fts_indexes_for_bucket()
+    elif ftsIndexMgr.action == "create_index_from_map":
+        ftsIndexMgr.create_fts_indexes_from_map_for_bucket()
     elif ftsIndexMgr.action == "run_queries":
         ftsIndexMgr.fts_query_runner()
     elif ftsIndexMgr.action == "delete_all_indexes":
@@ -989,6 +1223,8 @@ if __name__ == '__main__':
         ftsIndexMgr.item_count_check()
     elif ftsIndexMgr.action == "active_queries_check":
         ftsIndexMgr.active_queries_check()
+    elif ftsIndexMgr.action == "run_flex_queries":
+        ftsIndexMgr.run_flex_queries()
     else:
         print(
             "Invalid choice for action. Choose from the following - create_index | build_deferred_index | drop_all_indexes")
