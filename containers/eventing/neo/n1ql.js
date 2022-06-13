c978b3ad237a0cceb24aa1704aed9045322d6c26
Sujay Gad <gadsujay6026@gmail.com>
2022-06-13 13:19:05 +0530

    Fix issue for n1ql handler
    Change-Id: Ib38623b90c17e6d50afcd5e7662dc4b89ec4d406
    Reviewed-on: https://review.couchbase.org/c/sequoia/+/176086
    Reviewed-by: Chanabasappa Ghali <chanabasappa.ghali@couchbase.com>
    Tested-by: Sujay Gad <sujay.gad@couchbase.com>

--- containers/eventing/neo/n1ql.js
+++ containers/eventing/neo/n1ql.js
@@ -2,19 +2,19 @@ function OnUpdate(doc, meta) {
     try{
         if(meta.keyspace.scope_name == "scope_0"){
             if(meta.keyspace.collection_name == "coll0"){
-                UPSERT INTO n1ql_op.scope_0.coll0 ( KEY, VALUE ) VALUES ( $meta.id ,"n1ql insert");
+                UPSERT INTO n1ql_op.scope_0.coll0 ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
             }
             else if(meta.keyspace.collection_name == "coll1"){
-                UPSERT INTO n1ql_op.scope_0.coll1 ( KEY, VALUE ) VALUES ( $meta.id ,"n1ql insert");
+                UPSERT INTO n1ql_op.scope_0.coll1 ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
             }
             else if(meta.keyspace.collection_name == "coll2"){
-                UPSERT INTO n1ql_op.scope_0.coll2 ( KEY, VALUE ) VALUES ( $meta.id ,"n1ql insert");
+                UPSERT INTO n1ql_op.scope_0.coll2 ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
             }
             else if(meta.keyspace.collection_name == "coll3"){
-                UPSERT INTO n1ql_op.scope_0.coll3 ( KEY, VALUE ) VALUES ( $meta.id ,"n1ql insert");
+                UPSERT INTO n1ql_op.scope_0.coll3 ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
             }
             else if(meta.keyspace.collection_name == "coll4"){
-                UPSERT INTO n1ql_op.scope_0.coll4 ( KEY, VALUE ) VALUES ( $meta.id ,"n1ql insert");
+                UPSERT INTO n1ql_op.scope_0.coll4 ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
             }
             else{
                 UPSERT INTO $namespace ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
@@ -22,19 +22,19 @@ function OnUpdate(doc, meta) {
         }
         else{
             if(meta.keyspace.collection_name == "coll0"){
-                UPSERT INTO n1ql_op.scope_1.coll0 ( KEY, VALUE ) VALUES ( $meta.id ,"n1ql insert");
+                UPSERT INTO n1ql_op.scope_1.coll0 ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
             }
             else if(meta.keyspace.collection_name == "coll1"){
-                UPSERT INTO n1ql_op.scope_1.coll1 ( KEY, VALUE ) VALUES ( $meta.id ,"n1ql insert");
+                UPSERT INTO n1ql_op.scope_1.coll1 ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
             }
             else if(meta.keyspace.collection_name == "coll2"){
-                UPSERT INTO n1ql_op.scope_1.coll2 ( KEY, VALUE ) VALUES ( $meta.id ,"n1ql insert");
+                UPSERT INTO n1ql_op.scope_1.coll2 ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
             }
             else if(meta.keyspace.collection_name == "coll3"){
-                UPSERT INTO n1ql_op.scope_1.coll3 ( KEY, VALUE ) VALUES ( $meta.id ,"n1ql insert");
+                UPSERT INTO n1ql_op.scope_1.coll3 ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
             }
             else if(meta.keyspace.collection_name == "coll4"){
-                UPSERT INTO n1ql_op.scope_1.coll4 ( KEY, VALUE ) VALUES ( $meta.id ,"n1ql insert");
+                UPSERT INTO n1ql_op.scope_1.coll4 ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
             }
             else{
                 UPSERT INTO $namespace ( KEY, VALUE ) VALUES ( $$meta.id ,"n1ql insert");
@@ -50,42 +50,42 @@ function OnDelete(meta, options) {
     try{
         if(meta.keyspace.scope_name == "scope_0"){
             if(meta.keyspace.collection_name == "coll0"){
-                DELETE FROM n1ql_op.scope_0.coll0 USE KEYS $meta.id;
+                DELETE FROM n1ql_op.scope_0.coll0 USE KEYS $$meta.id;
             }
             else if(meta.keyspace.collection_name == "coll1"){
-                DELETE FROM n1ql_op.scope_0.coll1 USE KEYS $meta.id;
+                DELETE FROM n1ql_op.scope_0.coll1 USE KEYS $$meta.id;
             }
             else if(meta.keyspace.collection_name == "coll2"){
-                DELETE FROM n1ql_op.scope_0.coll2 USE KEYS $meta.id;
+                DELETE FROM n1ql_op.scope_0.coll2 USE KEYS $$meta.id;
             }
             else if(meta.keyspace.collection_name == "coll3"){
-                DELETE FROM n1ql_op.scope_0.coll3 USE KEYS $meta.id;
+                DELETE FROM n1ql_op.scope_0.coll3 USE KEYS $$meta.id;
             }
             else if(meta.keyspace.collection_name == "coll4"){
-                DELETE FROM n1ql_op.scope_0.coll4 USE KEYS $meta.id;
+                DELETE FROM n1ql_op.scope_0.coll4 USE KEYS $$meta.id;
             }
             else{
-                DELETE from $namespace USE KEYS $meta.id;
+                DELETE from $namespace USE KEYS $$meta.id;
             }
         }
         else{
             if(meta.keyspace.collection_name == "coll0"){
-                DELETE FROM n1ql_op.scope_1.coll0 USE KEYS $meta.id;
+                DELETE FROM n1ql_op.scope_1.coll0 USE KEYS $$meta.id;
             }
             else if(meta.keyspace.collection_name == "coll1"){
-                DELETE FROM n1ql_op.scope_1.coll1 USE KEYS $meta.id;
+                DELETE FROM n1ql_op.scope_1.coll1 USE KEYS $$meta.id;
             }
             else if(meta.keyspace.collection_name == "coll2"){
-                DELETE FROM n1ql_op.scope_1.coll2 USE KEYS $meta.id;
+                DELETE FROM n1ql_op.scope_1.coll2 USE KEYS $$meta.id;
             }
             else if(meta.keyspace.collection_name == "coll3"){
-                DELETE FROM n1ql_op.scope_1.coll3 USE KEYS $meta.id;
+                DELETE FROM n1ql_op.scope_1.coll3 USE KEYS $$meta.id;
             }
             else if(meta.keyspace.collection_name == "coll4"){
-                DELETE FROM n1ql_op.scope_1.coll4 USE KEYS $meta.id;
+                DELETE FROM n1ql_op.scope_1.coll4 USE KEYS $$meta.id;
             }
             else{
-                DELETE from $namespace USE KEYS $meta.id;
+                DELETE from $namespace USE KEYS $$meta.id;
             }
         }
     }
