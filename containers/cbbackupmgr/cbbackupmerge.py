be9f352cc9587972b6eb8f4a2e81b22fbdf2fa4c
Raghav S K <raghav.sk@couchbase.com>
2026-04-13 08:29:48 +0530

    CBQE-8891 : Adding a system test for backup restore using cbbackupmgr
    The test runs different operations with 3 backup strategies
    1. Full backup
    2. Incremental full backup
    3. Incremental periodic merge
    
    Change-Id: I39b756e18c6730ff57af51a7d71c7ba37d85d169
    Reviewed-on: https://review.couchbase.org/c/sequoia/+/243247
    Tested-by: Raghav S K <raghav.sk@couchbase.com>
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>
    Reviewed-by: <pavan.pb@couchbase.com>

--- containers/cbbackupmgr/cbbackupmerge.py
+++ containers/cbbackupmgr/cbbackupmerge.py
@@ -7,23 +7,24 @@ import select
 class CBBackupMerge():
     def run(self):
 
-        if len(sys.argv) != 5:
-            raise Exception("This script expects 5 arguments")
+        if len(sys.argv) != 6:
+            raise Exception("This script expects 5 arguments: hostname, ssh_username, ssh_password, backup_location, repo_name")
 
         # Hostname : sys.argv[1]
         # SSH Username : sys.argv[2]
         # SSH Password : sys.argv[3]
         # Backup Location : sys.argv[4]
+        # Repo Name : sys.argv[5]
 
-        command = "cd {0}/backup; sleep 1; find . -maxdepth 1 -type d -name [^\.]\* | sed 's:^\./::' | sort".format(sys.argv[4])
+        command = "cd {0}/{1}; sleep 1; find . -maxdepth 1 -type d -name [^\.]\* | sed 's:^\./::' | sort".format(sys.argv[4], sys.argv[5])
         output, std_err = self.execute_command(command)
 
         try:
             merge_start = output[0]
-            merge_end = output[len(output)-2]
+            merge_end = output[-1]
 
 
-            merge_command = "/opt/couchbase/bin/cbbackupmgr merge -a {0} -r backup --start {1} --end {2}".format(sys.argv[4], merge_start, merge_end)
+            merge_command = "/opt/couchbase/bin/cbbackupmgr merge -a {0} -r {1} --start {2} --end {3}".format(sys.argv[4], sys.argv[5], merge_start, merge_end)
             output,std_err = self.execute_command(merge_command)
 
             if output[0] != "Merge completed successfully" and std_err:
@@ -37,7 +38,7 @@ class CBBackupMerge():
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(sys.argv[1], username=sys.argv[2], password=sys.argv[3])
 
-        print "Executing : {0}".format(command)
+        print("Executing : {0}".format(command))
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command)
 
         output = ""
@@ -51,7 +52,7 @@ class CBBackupMerge():
 
         output = output.split("\n")
         for i in range(len(output)):
-            print output[i]
+            print(output[i])
 
         ssh.close()
 
