e08eb120d42ec3c11a6032a78fa0f740ca4cd557
Raghav S K <raghav.sk@couchbase.com>
2026-04-17 02:32:00 +0530

    CBQE-8991 : Adding chaos scenarios to backup restore system test
    - Added chaos scenarios to backup restore system test
    - System test stabilization fixes
    
    Change-Id: I4213f8bfa78665d6c5d3e8fe5ad6a00547a19c0e
    Reviewed-on: https://review.couchbase.org/c/sequoia/+/243518
    Tested-by: Raghav S K <raghav.sk@couchbase.com>
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>
    Reviewed-by: <pavan.pb@couchbase.com>

--- containers/cbbackupmgr/cbbackupmerge.py
+++ containers/cbbackupmgr/cbbackupmerge.py
@@ -16,7 +16,7 @@ class CBBackupMerge():
         # Backup Location : sys.argv[4]
         # Repo Name : sys.argv[5]
 
-        command = "cd {0}/{1}; sleep 1; find . -maxdepth 1 -type d -name [^\.]\* | sed 's:^\./::' | sort".format(sys.argv[4], sys.argv[5])
+        command = "cd {0}/{1}; sleep 1; find . -maxdepth 1 -type d -name [^\.]\* | sed 's:^\./::' | grep -v '^logs$' | sort".format(sys.argv[4], sys.argv[5])
         output, std_err = self.execute_command(command)
 
         try:
@@ -27,8 +27,9 @@ class CBBackupMerge():
             merge_command = "/opt/couchbase/bin/cbbackupmgr merge -a {0} -r {1} --start {2} --end {3}".format(sys.argv[4], sys.argv[5], merge_start, merge_end)
             output,std_err = self.execute_command(merge_command)
 
-            if output[0] != "Merge completed successfully" and std_err:
-                raise Exception("cbbackupmgr merge command results in an error : %s", std_err)
+            stderr_content = std_err.read().decode().strip() if std_err else ""
+            if len(output) > 0 and output[0] != "Merge completed successfully" and stderr_content:
+                raise Exception("cbbackupmgr merge command results in an error : %s" % stderr_content)
         except Exception as e:
             raise Exception(str(e))
 
@@ -50,7 +51,7 @@ class CBBackupMerge():
                     tmp = ssh_stdout.channel.recv(1024)
                     output += tmp.decode()
 
-        output = output.split("\n")
+        output = output.strip().split("\n")
         for i in range(len(output)):
             print(output[i])
 
